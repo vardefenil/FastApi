74871a4f78a280688947ae7a88e86220169a9293
fenil varde <vardefenil6@gmail.com>
2026-06-29 11:03:56 +0530

    nested_model included

---
+++ 5-nested_models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class Patient(BaseModel):
+
+    name: str
+    gender: str
+    age: int
+    address: Address
+
+address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'nitish', 'gender': 'male', 'age': 35, 'address': address1}
+
+patient1 = Patient(**patient_dict)
+
+temp = patient1.model_dump(exclude='name')
+print(temp)
+
+print(type(temp))
